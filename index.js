b07bcec28654e3673085f8ac548c657886a2f623
micsoni <micsoni@gmail.com>
2020-02-26 16:59:31 +0100

    added heroku database and db.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
+const db = require("./db");
 const app = express();
 const port = process.env.PORT || 4000;
 
-const { Router } = express;
-const router = new Router();
-router.get("/", (request, response) =>
-  response.send("Welcome to the homepage!")
-);
+// const { Router } = express;
+// const router = new Router();
+app.get("/", (request, response) => response.send("Welcome to the homepage!"));
 
-app.use(router);
+//app.use(router);
 app.listen(port, () => console.log("listening on port " + port));
