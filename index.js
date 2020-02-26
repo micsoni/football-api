a5e36d21cdd8983bdbcab2be6e68e5d55781839a
micsoni <micsoni@gmail.com>
2020-02-26 17:22:48 +0100

    created team model

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const db = require("./db");
 const app = express();
 const port = process.env.PORT || 4000;
+const Team = require("./Team/model");
 
 // const { Router } = express;
 // const router = new Router();
