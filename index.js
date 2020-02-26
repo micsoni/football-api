eb4199d57eaefa811058e3190736cffffbb37650
micsoni <micsoni@gmail.com>
2020-02-26 16:23:40 +0100

    added get route for '/'

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
+const { Router } = express;
+const router = new Router();
+router.get("/", (request, response) =>
+  response.send("Welcome to the homepage!")
+);
+
+app.use(router);
 app.listen(port, () => console.log("listening on port " + port));
