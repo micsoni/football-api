26f69d63977f1f82bf3612a27369802ead328e75
micsoni <micsoni@gmail.com>
2020-02-27 10:10:33 +0100

    added get team by id endpoint

--- Team/router.js
+++ Team/router.js
@@ -19,4 +19,15 @@ router.post("/teams", (req, res, next) => {
     .catch(next);
 });
 
+router.get("/teams/:teamId", (req, res, next) => {
+  Team.findByPk(req.params.teamId)
+    .then(team => {
+      if (!team) {
+        res.status(404).end();
+      } else {
+        res.json(team);
+      }
+    })
+    .catch(next);
+});
 module.exports = router;
